0d0ea9e87822d8c0dd466051c4a844c6f5aae8b8
hero <361754100@qq.com>
2020-05-20 15:06:57 +0800

    完善配置

--- src/main/java/com/hro/core/cloudverifyapi/controller/RsaController.java
+++ src/main/java/com/hro/core/cloudverifyapi/controller/RsaController.java
@@ -1,13 +1,25 @@
 package com.hro.core.cloudverifyapi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RsaController {
 
-    @GetMapping("/test")
+    private static Logger logger = LoggerFactory.getLogger(RsaController.class);
+
+
+    @GetMapping("/rsa/test")
     public boolean test() {
-        return false;
+//        // 故意延迟测试hystrixcommand
+        try {
+            Thread.sleep(2*1000l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        logger.debug("rsa test...");
+        return true;
     }
 }
